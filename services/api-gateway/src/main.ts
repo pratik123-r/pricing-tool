2fc828b068da06a7bacb6fac9f75e290099fe945
Pratik Rajkotiya <pratikr@gorentals.com>
2025-11-08 17:39:19 +0530

    add import into docker

--- services/api-gateway/src/main.ts
+++ services/api-gateway/src/main.ts
@@ -1,5 +1,4 @@
 import 'reflect-metadata';
-import 'tsconfig-paths/register';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
